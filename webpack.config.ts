18d37441af0e72fad426baf2b92bb8c06a1bf4d8
designneexx <webmasterondesign@gmail.com>
2024-07-04 12:17:02 +0300

    поправил сборку

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,11 +2,10 @@ import path from 'path';
 import ESLintPlugin from 'eslint-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import TerserWebpackPlugin from 'terser-webpack-plugin';
-import webpack from 'webpack';
+import webpack, { DefinePlugin } from 'webpack';
 import packageJson from './package.json';
 
 import 'webpack-dev-server';
-import DotenvWebpackPlugin from 'dotenv-webpack';
 
 type ConfigParams = Record<string, string>;
 
@@ -101,9 +100,10 @@ export default (
             new ESLintPlugin({
                 extensions: ['js', 'mjs', 'jsx', 'ts', 'tsx']
             }),
-            new DotenvWebpackPlugin({
-                path: './.env', // Path to .env file (this is the default)
-                safe: true // load .env.example (defaults to "false" which does not use dotenv-safe)
+            new DefinePlugin({
+                'process.env': JSON.stringify({
+                    BASE_API_URL: process.env.BASE_API_URL
+                })
             }),
             new ModuleFederationPlugin({
                 filename: 'remoteEntry.js',
